373d90793d500e2672b43cfd04adfad8f80a19df
Isaiah Aniemeka <isaiah.aniemeka@gmail.com>
2023-02-03 01:15:48 -0500

    Making website responsive

--- js/stickynav.js
+++ js/stickynav.js
@@ -8,12 +8,10 @@ var navbar = document.getElementById("navbar");
 
 function scrollFunction(){
     if(document.body.scrollTop > 30 || document.documentElement.scrollTop > 30){
-        document.getElementById("navbar").style.padding = "10px 10%";
-        document.getElementById("navbar").style.opacity = "0.8";
+        document.getElementById("navbar").style.padding = "10px 10%";      
     }
     else{
         document.getElementById("navbar").style.padding = "40px 10%";
-        document.getElementById("navbar").style.opacity = "1";
     }
 }
 
